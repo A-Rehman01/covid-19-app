0943906715a6f5457a51572598350e9366ded160
A-Rehman01 <59839069+A-Rehman01@users.noreply.github.com>
2020-07-04 12:23:45 -0700

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import FlagIcon from '@material-ui/icons/Flag';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 import PublicIcon from '@material-ui/icons/Public';
 
 const useStyles = makeStyles({
